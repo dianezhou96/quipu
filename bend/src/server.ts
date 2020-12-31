0feea9f6bc96468b5dc8388fc98c76de9ae80d65
Alex Huang <alexh95@mit.edu>
2020-12-31 00:09:34 -0800

    Add DB connection and scaffold MonthlyWidget

--- bend/src/server.ts
+++ bend/src/server.ts
@@ -9,16 +9,24 @@ const cookieParser = require('cookie-parser'),
   bodyParser = require('body-parser'),
   http = require('http').Server(app),
   router = express.Router(),
-  session = require('express-session');
+  session = require('express-session'),
+  {Client} = require('pg');
+
+const db = new Client({
+  user: config.db_user,
+  host: config.db_host,
+  database: config.db_name,
+  password: config.db_pw,
+  port: config.db_port
+});
 
 
-//import { Request, Response, NextFunction } from 'express';
-
-let MonthlyWidget = require('./widgets/MonthlyWidget')();
+let MonthlyWidget = require('./widgets/MonthlyWidget')(db);
 
 app.use(morgan('dev'));
 // serve SPA scaffold
-app.use(express.static('../fend/dist'))
+app.use(express.static('../fend/dist'));
+app.use('/monthly_widget', MonthlyWidget);
 
 //app.use(cookieParser(config.sessionSecret));
 //app.use(bodyParser.json());
@@ -30,5 +38,11 @@ app.use(express.static('../fend/dist'))
 //app.use(passport.initialize());
 //app.use(passport.session());
 
-console.log('Server started, listening on port', config.port);
-app.listen(config.port);
+db.connect((err: Error) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server started, listening on port', config.port);
+    app.listen(config.port);
+  }
+});
--- bend/src/widgets/MonthlyWidget.ts
+++ bend/src/widgets/MonthlyWidget.ts
@@ -1,2 +1,18 @@
-export = function () {
+import { Request, Response, NextFunction } from 'express';
+import { QueryResult, QueryResultRow, Client} from 'pg';
+
+const express = require('express'),
+      router = express.Router();
+
+module.exports = function (db: Client) {
+  router.get('/', function(req: Request, res: Response){
+    db.query('SELECT * from balances', (err: Error, db_res: QueryResult<QueryResultRow>) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      res.send(db_res.rows);
+    });
+  });
+  return router
 };
